6bd2c3091d56c3af74ee2d3c27155d97cc7b0105
Gregorius555 <128634116+Gregorius555@users.noreply.github.com>
2023-03-27 22:11:17 +0200

    Update app/about/page.tsx with custom Metadata type

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,46 +1,42 @@
-import type { Metadata } from 'next';
-import {
-  ArrowIcon,
-} from 'components/icons';
+import React from 'react';
+import { ArrowIcon } from 'components/icons';
+
+// Define your own Metadata type
+type Metadata = {
+  title: string;
+  description: string;
+};
 
 export const metadata: Metadata = {
   title: 'About',
   description: 'Business enthusiast',
 };
 
-export default function AboutPage() {
+const AboutPage: React.FC = () => {
   return (
     <section>
       <h1 className="font-bold text-3xl font-serif">About Me</h1>
       <p className="my-5 text-neutral-800 dark:text-neutral-200">
-        Hello! I'm Gergely, a dynamic <b>business enthusiast</b> with a diverse skill set 
-        and a passion for leadership, management, and value creation.
+        Hello! I'm Gergely, a dynamic <b>business enthusiast</b> with a diverse skill set and a passion for leadership, management, and value creation.
       </p>
       <p>
-        My experiences range from restructuring a <b>natural gas pricing</b> team during 
-        a merger to auditing Hungary's largest companies, always with a 
-        focus on driving success and innovation.
-      </p><br></br>
-      <hr /><br></br>
+        My experiences range from restructuring a <b>natural gas pricing</b> team during a merger to auditing Hungary's largest companies, always with a focus on driving success and innovation.
+      </p>
+      <br />
+      <hr />
+      <br />
       <p>
-        My interests span strategic planning, <b>business valuation</b>, stock exchange, 
-        hedge funds, and beyond. I pride myself on blending vision with emotional 
-        intelligence, fostering environments where teams can excel. By navigating 
-        change, making informed decisions, and communicating effectively, I connect 
-        deeply with others and create strong collaborative bonds.
-      </p><br></br>
+        My interests span strategic planning, <b>business valuation</b>, stock exchange, hedge funds, and beyond. I pride myself on blending vision with emotional intelligence, fostering environments where teams can excel. By navigating change, making informed decisions, and communicating effectively, I connect deeply with others and create strong collaborative bonds.
+      </p>
+      <br />
       <p>
-        Resilience, integrity, and continuous learning are the cornerstones of my 
-        leadership approach. I stay focused on <b>long-term goals</b>, act with honesty and 
-        transparency, and foster a culture that values growth and development, both for 
-        myself and my team.
-      </p><br></br>
+        Resilience, integrity, and continuous learning are the cornerstones of my leadership approach. I stay focused on <b>long-term goals</b>, act with honesty and transparency, and foster a culture that values growth and development, both for myself and my team.
+      </p>
+      <br />
       <p>
-        I invite you to <b>connect with me</b> and explore new opportunities together. 
-        Don't forget to check out my blog for insights into my passions and interests. 
-        Let's expand our professional networks and make a lasting impact on the world 
-        around us!
-      </p><br></br>
+        I invite you to <b>connect with me</b> and explore new opportunities together. Don't forget to check out my blog for insights into my passions and interests. Let's expand our professional networks and make a lasting impact on the world around us!
+      </p>
+      <br />
       <div className="flex flex-col gap-2 md:flex-row md:gap-2">
         <a
           className="flex items-center text-neutral-500 dark:text-neutral-400 hover:text-neutral-700 dark:hover:text-neutral-200 transition-all"
@@ -54,4 +50,6 @@ export default function AboutPage() {
       </div>
     </section>
   );
-}
+};
+
+export default AboutPage;
