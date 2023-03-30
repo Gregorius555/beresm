5c4f85f641972d622b90b3783c018b717336ad56
Gregorius555 <128634116+Gregorius555@users.noreply.github.com>
2023-03-30 22:13:31 +0200

    blockchain-crypto_v2

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -34,7 +34,7 @@ const computedFields = {
       url: `https://beresm.com/blog/${doc._raw.flattenedPath}`,
       author: {
         '@type': 'Person',
-        name: 'Lee Robinson',
+        name: 'Gergely Béres-Molnár',
       },
     }),
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './app/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
-    './content/**/*.mdx',
+    './content/*.mdx',
   ],
   theme: {
     extend: {
