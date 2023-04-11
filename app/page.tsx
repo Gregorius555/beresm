45f2fa908eff27f6224b6fe2900e2fc878b6b226
Gregorius555 <128634116+Gregorius555@users.noreply.github.com>
2023-04-11 18:40:19 +0200

    personal-brand

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default async function HomePage() {
             className="flex items-center gap-2"
           >
             <TwitterIcon />
-            {`10 tweets all time`} {/* Add this line */}
+            {`11 tweets all time`} {/* Add this line */}
           </a>
           <Link href="/blog" className="flex items-center">
             <ViewsIcon />
