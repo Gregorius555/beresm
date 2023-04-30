e9abc8c11cb744645795042e8a54ae2bdfb48b9a
Gregorius555 <128634116+Gregorius555@users.noreply.github.com>
2023-04-30 19:12:00 +0200

    tweet 16

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default async function HomePage() {
             className="flex items-center gap-2"
           >
             <TwitterIcon />
-            {`11 tweets all time`} {/* Add this line */}
+            {`16 tweets all time`} {/* Add this line */}
           </a>
           <Link href="/blog" className="flex items-center">
             <ViewsIcon />
