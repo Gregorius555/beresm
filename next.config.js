58cdf0baac4585dd79c46a5d52bd4323f37dfc1f
Gergely Béres-Molnár <beresm@Gergelys-MacBook-Air.local>
2023-08-27 19:06:39 +0200

    app-api

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ const nextConfig = {
   },
   redirects() {
     try {
-      return get('redirects') || [];
+      return get('redirects');
     } catch {
       return [];
     }
@@ -39,7 +39,7 @@ const ContentSecurityPolicy = `
     default-src 'self' vercel.live;
     script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
     style-src 'self' 'unsafe-inline';
-    img-src * blob: data: localhost:*;
+    img-src * blob: data:;
     media-src 'none';
     connect-src *;
     font-src 'self' data:;
